1cdb448f780fc873f1d41d9719e55742e0dabc99
Dmitry <skifcha200211@mail.ru>
2023-06-08 19:57:53 +0500

    Changes UserController

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -1,16 +1,12 @@
 import jwt from "jsonwebtoken";
 import bcrypt from "bcrypt";
 
-import { validationResult } from "express-validator";
 import UserModel from "../models/User.js";
 
 const SECRET_KEY = "lolkekcheburek283598203958205982";
 
 export const register = async (req, res) => {
   try {
-    const errors = validationResult(req);
-    if (!errors.isEmpty()) return res.status(400).json(errors.array());
-
     const password = req.body.password;
     const salt = await bcrypt.genSalt(10);
     const hash = await bcrypt.hash(password, salt);
